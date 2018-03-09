51fc34e8e7cb6be8d85b1c711df54d6fc4894ccb
Lars Hupel <lars.hupel@mytum.de>
2018-03-09 11:32:42 +0100

    tuned

--- thy/Big_Step_Total.thy
+++ thy/Big_Step_Total.thy
@@ -2,13 +2,15 @@ theory Big_Step_Total
 imports Semantic_Extras
 begin
 
-lemma evaluate_list_total:
+context begin
+
+private lemma evaluate_list_total0:
   fixes s :: "'a state"
   assumes "\<And>e env s'::'a state. e \<in> set es \<Longrightarrow> clock s' \<le> clock s \<Longrightarrow> \<exists>s'' r. evaluate True env s' e (s'', r)"
   shows "\<exists>s' r. evaluate_list True env s es (s', r)"
 using assms proof (induction es arbitrary: env s)
   case Nil
-  then show ?case by (metis evaluate_match_evaluate_list_evaluate.empty)
+  show ?case by (metis evaluate_match_evaluate_list_evaluate.empty)
 next
   case (Cons e es)
   then obtain s' r where e: "evaluate True env s e (s', r)"
@@ -34,13 +36,13 @@ next
     qed
 qed
 
-lemma evaluate_match_total:
+private lemma evaluate_match_total0:
   fixes s :: "'a state"
   assumes "\<And>p e env s'::'a state. (p, e) \<in> set pes \<Longrightarrow> clock s' \<le> clock s \<Longrightarrow> \<exists>s'' r. evaluate True env s' e (s'', r)"
   shows "\<exists>s' r. evaluate_match True env s v pes v' (s', r)"
 using assms proof (induction pes arbitrary: env s)
   case Nil
-  then show ?case by (metis mat_empty)
+  show ?case by (metis mat_empty)
 next
   case (Cons pe pes)
   then obtain p e where "pe = (p, e)" by force
@@ -115,7 +117,7 @@ proof -
             proof (cases "do_con_check (c env) cn (length es)")
               case True
               have "\<exists>s' vs. evaluate_list True env s (rev es) (s', vs)"
-                apply (rule evaluate_list_total)
+                apply (rule evaluate_list_total0)
                 apply (rule less)
                 unfolding Con
                 apply auto
@@ -149,7 +151,7 @@ proof -
         next
           case (App op es)
           have "\<exists>s' vs. evaluate_list True env s (rev es) (s', vs)"
-            apply (rule evaluate_list_total)
+            apply (rule evaluate_list_total0)
             apply (rule less)
             unfolding App apply (auto simp: less_eq_Suc_le)
             apply (rule size_list_estimation')
@@ -353,7 +355,7 @@ proof -
                   case (Rraise exn)
 
                   have "\<exists>s'' r. evaluate_match True env s' exn pes exn (s'', r)"
-                    apply (rule evaluate_match_total)
+                    apply (rule evaluate_match_total0)
                     apply (rule less)
                     using Handle clock apply (auto simp: less_eq_Suc_le)
                     apply (rule trans_le_add1)
@@ -389,7 +391,7 @@ proof -
               case (Rval v)
 
               have "\<exists>s'' r. evaluate_match True env s' v pes (Conv (Some (''Bind'', TypeExn (Short ''Bind''))) []) (s'', r)"
-                apply (rule evaluate_match_total)
+                apply (rule evaluate_match_total0)
                 apply (rule less)
                 unfolding Mat using clock apply (auto simp: less_eq_Suc_le)
                 apply (rule trans_le_add1)
@@ -458,6 +460,92 @@ proof -
             unfolding \<open>e = _\<close>
             by (fastforce intro: evaluate_match_evaluate_list_evaluate.intros)
         qed (fastforce intro: evaluate_match_evaluate_list_evaluate.intros)+
+    qed
+qed
+
+end
+
+text \<open>
+  The following are pretty much the same proofs as above, but without additional assumptions;
+  instead using @{thm [source=true] evaluate_total} directly.
+\<close>
+
+lemma evaluate_list_total: "\<exists>s' r. evaluate_list True env s es (s', r)"
+proof (induction es arbitrary: env s)
+  case Nil
+  show ?case by (metis evaluate_match_evaluate_list_evaluate.empty)
+next
+  case (Cons e es)
+  obtain s' r where e: "evaluate True env s e (s', r)"
+    by (metis evaluate_total)
+  show ?case
+    proof (cases r)
+      case (Rval v)
+      have "\<exists>s'' r. evaluate_list True env s' es (s'', r)"
+        using Cons by auto
+      then obtain s'' r where "evaluate_list True env s' es (s'', r)"
+        by auto
+
+      with e Rval show ?thesis
+        by (cases r)
+           (metis evaluate_match_evaluate_list_evaluate.cons1 evaluate_match_evaluate_list_evaluate.cons3)+
+    next
+      case Rerr
+      with e show ?thesis
+        by (metis evaluate_match_evaluate_list_evaluate.cons2)
+    qed
+qed
+
+lemma evaluate_match_total: "\<exists>s' r. evaluate_match True env s v pes v' (s', r)"
+proof (induction pes arbitrary: env s)
+  case Nil
+  show ?case by (metis mat_empty)
+next
+  case (Cons pe pes)
+  then obtain p e where "pe = (p, e)" by force
+
+  show ?case
+    proof (cases "allDistinct (pat_bindings p [])")
+      case distinct: True
+      show ?thesis
+        proof (cases "pmatch (c env) (refs s) p v []")
+          case No_match
+
+          have "\<exists>s' r. evaluate_match True env s v pes v' (s', r)"
+            by (rule Cons)
+          then obtain s' r where "evaluate_match True env s v pes v' (s', r)"
+            by auto
+
+          show ?thesis
+            unfolding \<open>pe = _\<close>
+            apply (intro exI)
+            apply (rule mat_cons2)
+            apply safe
+            by fact+
+        next
+          case Match_type_error
+          then show ?thesis
+            unfolding \<open>pe = _\<close> by (metis mat_cons3)
+        next
+          case (Match env')
+
+          have "\<exists>s' r. evaluate True (env \<lparr> sem_env.v := (nsAppend (alist_to_ns env') (sem_env.v env)) \<rparr>) s e (s', r)"
+            by (metis evaluate_total)
+          then obtain s' r where "evaluate True (env \<lparr> sem_env.v := (nsAppend (alist_to_ns env') (sem_env.v env)) \<rparr>) s e (s', r)"
+            by auto
+
+          show ?thesis
+            unfolding \<open>pe = _\<close>
+            apply (intro exI)
+            apply (rule mat_cons1)
+            apply safe
+            apply fact+
+            done
+        qed
+    next
+      case False
+      then show ?thesis
+        unfolding \<open>pe = _\<close> by (metis mat_cons4)
     qed
 qed
 
